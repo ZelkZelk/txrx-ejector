5955261e42392656d12496337aedbc10746b8c98
klez <felipeklez@gmail.com>
2023-08-19 22:27:28 -0400

    add makefile setup.sh

--- setup.sh
+++ setup.sh
@@ -31,3 +31,15 @@ then
         | sed "s/HANDLERS_DIR: backend/HANDLERS_DIR: \.\.\/backend/g"   \
         | sed "s/context: \.\//context: \.\/\n      working_dir: \/txrx/g" > docker-compose.yml
 fi
+
+if [[ ! -f Makefile ]] ;
+then
+    project="${PWD##*/}"
+    cat txrx/Makefile                                                 \
+        | sed "s/txrx/$project/g"                                      \
+        | tr '\n' '\r'                                                  \
+        | sed "s/node_clean:/node_clean:\r\trm -rf backend\/dist backend\/node_modules frontend\/dist frontend\/node_modules\r\tcd backend \&\& npm i \&\& cd \.\.\/frontend \&\& npm i\r\rws:/g"   \
+        | sed "s/ws:.*ws:/ws:/"   \
+        | tr '\r' '\n' \
+        > Makefile
+fi
